93b5bde66e7e1032729cb8b7c583d1ae547e55f5
baf62495 <baf62495@aol.com>
2019-06-07 19:32:05 -0400

    added question data

--- data.js
+++ data.js
@@ -1,14 +1,122 @@
 const STORE = [
 	{
-		question: '',
+		question: 'What is the first year the Devils qualified for the Stanley Cup Playoffs?',
 		answerChoices: [
-			'',
-			'',
-			'',
-			''
+			'2000',
+			'1982',
+			'1988',
+			'1995'
 			],
-		correctAnswer: '',
-		answerStatement: '',
-		funFact: ''
+		correctAnswer: '1988',
+		answerStatement: `The first year the Devils qualified for the Stanley Cup Playoffs was in ${this.correctAnswer}!`,
+		funFact: 'The Devils clinched a playoff spot in the last game of the 1988 regular season, with a win against the Blackhawks.'
 	},
+	{
+		question: 'Who scored the Stanley Cup winning goal for the Devils in 1995?',
+		answerChoices: [
+			'Scott Stevens',
+			'Claude Lemieux',
+			'Neal Broten',
+			'Stephan Richer'
+			],
+		correctAnswer: 'Neal Broten',
+		answerStatement: `${this.correctAnswer} scored the Stanley Cup winning goal for the Devils in 1995!`,
+		funFact: 'Neal Broten became one of the few American-born players to score a Cup winning goal.'
+	},
+	{
+		question: 'In which of the following seasons, did Martin Brodeur win the Calder Trophy?',
+		answerChoices: [
+			'1995-1996',
+			'1999-2000',
+			'1994-1995',
+			'1993-1994'
+			],
+		correctAnswer: '1993-1994',
+		answerStatement: `Martin Brodeur won the Calder Trophy in ${this.correctAnswer}!`,
+		funFact: 'Martin Brodeur and Scott Gomez are the only players in New Jersey Devils history to ever win the Calder Trophy.'
+	},
+	{
+		question: 'Who was the coach of the Devils\' 2000 Stanley Cup winning team?',
+		answerChoices: [
+			'Jacques Lemaire',
+			'Pat Burns',
+			'Lou Lamoriello',
+			'Larry Robinson'
+			],
+		correctAnswer: 'Larry Robinson',
+		answerStatement: `${this.correctAnswer} was the coach of the Devils' Stanley Cup winning team!`,
+		funFact: 'Larry Robinson, Hall of Fame defenseman, was nicknamed "Big Bird", during his playing days in Montreal.'
+	},
+	{
+		question: 'In how many games did the Devils win the 1995 Stanley Cup Final?',
+		answerChoices: [
+			'5',
+			'4',
+			'6',
+			'7'
+			],
+		correctAnswer: '4',
+		answerStatement: `The Devils swept the 1995 Stanley Cup Finals in ${this.correctAnswer} games!`,
+		funFact: 'In the 1995 season, the Devils captured their first Stanley Cup in a 4 game sweep, of the heavily favored Detroit Red Wings.'
+	},
+	{
+		question: 'At the start of the Devils franchise, where did they play and what was their team name?',
+		answerChoices: [
+			'Kansas City Scouts',
+			'Colorado Rockies',
+			'New Jersey Devils',
+			'Brooklyn Americans'
+			],
+		correctAnswer: 'Kansas City Scouts',
+		answerStatement: `In 1974, the Devils\' franchise started out as the ${this.correctAnswer}!`,
+		funFact: 'Two years after the start of the franchise, the Scouts moved to Denver and became the Colorado Rockies.'
+	},
+	{
+		question: 'In the 1998 NHL Entry Draft, what round did the Devils select Scott Gomez?',
+		answerChoices: [
+			'3rd',
+			'6th',
+			'1st',
+			'4th'
+			],
+		correctAnswer: '1st',
+		answerStatement: `The Devils selected Scott Gomez with their ${this.correctAnswer} round pick in the 1998 Entry Draft!`,
+		funFact: 'Scott justified the Devils’ decision by scoring 108 points in 58 games for the Tri-City Americans of the WHL.'
+	},
+	{
+		question: 'Who was the Captain of the New Jersey Devils in 2003?',
+		answerChoices: [
+			'Patrik Elias',
+			'Jamie Langenbrunner',
+			'Scott Niedermayer',
+			'Scott Stevens'
+			],
+		correctAnswer: 'Scott Stevens',
+		answerStatement: `The Captain of the New Jersey Devils in 2003 was ${this.correctAnswer}!`,
+		funFact: 'Scott Stevens captained the New Jersey Devils from 1992 to 2004, for a total of 12 straight seasons.'
+	},
+	{
+		question: 'The New Jersey Devils held a 20 game unbeaten streak against which rivals?',
+		answerChoices: [
+			'New York Rangers',
+			'Philadelphia Flyers',
+			'New York Islanders',
+			'Carolina Hurricanes'
+			],
+		correctAnswer: 'New York Rangers',
+		answerStatement: `The Devils held a 20 game unbeaten streak against the ${this.correctAnswer}!`,
+		funFact: 'The Devils scored 72 goals to the Rangers 38, and managed 14 wins, six ties, and one shutout over this span.'
+	},
+	{
+		question: 'What was the last year the color green was a part of the Devils wardrobe?',
+		answerChoices: [
+			'1995',
+			'1988',
+			'1992',
+			'2000'
+			],
+		correctAnswer: '1992',
+		answerStatement: `The Devils wore the color green as a part of their wardrobe for the last time in ${this.correctAnswer}!`,
+		funFact: 'The green on the jerseys was removed and replaced with black, and less stripes appeared on the sweater as well.'
+	}
 ];
