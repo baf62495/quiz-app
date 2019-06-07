558296bdc238141a6d322efb064cabd6a296f40b
baf62495 <baf62495@aol.com>
2019-06-07 11:45:04 -0400

    js psuedo & functon stubs

--- index.js
+++ index.js
@@ -5,15 +5,18 @@ let userScore = 0;
 function generateQuestionHtml() {
 	// starts on display: none
 	// generate html based on question objects
+	console.log(`generateQuestionHtml ran`);
 }
 
 function incrementQuestionNumber() {
 	// increment question number
 	// take question number and add 1 to index (question 1 is 1, not 0)
+	console.log(`incrementQuestionNumber ran`);
 }
 
 function incrementUserScore() {
 	// increment userScore
+	console.log(`incrementUserScore ran`);
 }
 
 function startQuiz() {
@@ -22,16 +25,19 @@ function startQuiz() {
 		// remove main screen html
 		// show question form html
 		// render question number(1)
+	console.log(`startQuiz ran`);
 }
 
 function renderQuestion() {
 	// render (generateQuestionHtml)
+	console.log(`renderQuestion ran`);
 }
 
 function userSubmitAnswer() {
 	// user submits answer
 		// when user submits answer =>
 			// check answer
+	console.log(`userSubmitAnswer ran`);
 }
 
 function ifAnswerIsCorrect() {
@@ -40,6 +46,7 @@ function ifAnswerIsCorrect() {
 		// render correct feedback html
 		// render button(next)
 		// render fun fact in html
+	console.log(`ifAnswerIsCorrect ran`);
 }
 
 function ifAnswerIsWrong() {
@@ -48,6 +55,13 @@ function ifAnswerIsWrong() {
 		// add user choice in html
 		// render button(next)
 		// render fun fact in html
+	console.log(`ifAnswerIsWrong ran`);
+}
+
+function updateScore() {
+	// increment score =>
+		// change html to show current score
+	console.log(`updateScore ran`);
 }
 
 function renderNextQuestion() {
@@ -55,6 +69,7 @@ function renderNextQuestion() {
 		// on button(next) click =>
 			// render next question html
 			// increment question # by 1
+	console.log(`renderNextQuestion ran`);
 }
 
 // repeat until last question
@@ -70,6 +85,7 @@ function renderResults() {
 			// if fail
 				// render fail feedback html
 				// add score to feedback html
+	console.log(`renderResults ran`);
 }
 
 
@@ -78,6 +94,7 @@ function createQuiz() {
   renderQuestion();
   userSubmitAnswer();
   renderNextQuestion();
+  console.log(`createQuiz ran`);
 }
 
 $(createQuiz);
